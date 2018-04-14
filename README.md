772de04365bb12e5c9c960b51b8d783bbb48cd52
david njoroge <njorogedavid13@gmail.com>
2018-04-14 15:24:50 +0300

    *add commander module for making terminal commands *add .gitignore and updated the package json

---
+++ README.md
@@ -0,0 +1,6 @@
+## Design Pack
+
+#### description
+
+a web design starter pack containing index.html file with css and js files linked and optional jquery and bootstrap libraries.
+
--- cli.js
+++ cli.js
@@ -3,12 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const program = require('commander');
-
-//  fs.readFile(path.resolve(__dirname,'texts.txt'),(err,data)=>{
-//      let readData = data.toString()
-//      console.log(readData);
-//  })
-
+const mkdirp = require('mkdirp')
  let cmdValue
 program
   .option('--no-sauce', 'Remove sauce')
@@ -17,6 +12,23 @@ program
      cmdValue = cmd;
      envValue = env;
   });
+
 program.parse(process.argv)
 console.log('my command',cmdValue);
-// if(cmdValue==undefined)
+if(cmdValue!=undefined){
+  main(cmdValue)
+}
+
+function main(cmdValue){
+  mkdir(cmdValue,()=>{
+
+  })
+}
+
+function mkdir(path, fn) {
+  mkdirp(path, 0755, function(err){
+    if (err) throw err;
+    console.log('   \033[36mcreate\033[0m : ' + path);
+    fn && fn();
+  });
+}
