57271e1b51ac12a5d93da45011f4f52e492b0f0c
david njoroge <njorogedavid13@gmail.com>
2018-05-20 21:32:43 +0300

    update how the files are created

--- cli.js
+++ cli.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 const path = require('path');
 const program = require('commander');
 const mkdirp = require('mkdirp')
-const cmd = require('node-cmd')
+const TEMPLATE_DIR = path.join(__dirname, 'design-template')
+const MODE_0666 = parseInt('0666', 8)
 
 // global variables
  let cmdValue
 program
-  .option('--no-sauce', 'Remove sauce')
   .arguments('<cmd> [env]')
   .action(function (cmd, env) {
      cmdValue = cmd;
@@ -17,20 +17,38 @@ program
   });
 
 program.parse(process.argv)
-console.log('Created design pack: ',cmdValue);
+
+
 if(cmdValue!=undefined){
   main(cmdValue)
+}else{
+  console.log("please write the app name ");
+  
 }
 
 //this is where the code starts, where 'cmdValue' is the terminal parameter
 function main(cmdValue){
-  // mkdir(cmdValue,()=>{
+  var destinationPath = program.args.shift() || '.'
 
-  // })
-  cmd.get(
-    `cp -r design-template ${cmdValue} && cd ${cmdValue}`
-  )
+  mkdir(cmdValue,()=>{
 
+  })
+
+  // const files = ['bootstrap.css','index.html','jquery-3.3.1.min.js','script.js','style.css']
+
+  for(let file of fs.readdirSync(path.join(TEMPLATE_DIR))){
+    copyTemplate(path.join(file),path.join(`${cmdValue}/${file}`))
+    
+  }
+}
+
+function copyTemplate (from, to) {
+  write(to, fs.readFileSync(path.join(TEMPLATE_DIR, from), 'utf-8'))
+}
+
+function write (file, str, mode) {
+  fs.writeFileSync(file, str, { mode: mode || MODE_0666 })
+  console.log('   \x1b[36mcreate\x1b[0m : ' + file)
 }
 
 function mkdir(path, fn) {
